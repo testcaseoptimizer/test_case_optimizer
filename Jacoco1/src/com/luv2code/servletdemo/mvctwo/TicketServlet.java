93b41494ec50eec655ab3af12bc0376e316cfde9
testcaseoptimizer <139108179+testcaseoptimizer@users.noreply.github.com>
2023-07-19 11:31:02 +0530

    Update TicketServlet.java

--- Jacoco1/src/com/luv2code/servletdemo/mvctwo/TicketServlet.java
+++ Jacoco1/src/com/luv2code/servletdemo/mvctwo/TicketServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MvcDemoServletTwo
+ * Servlet implementation class Mvchelloall
  */
 @WebServlet("/TicketServlet")
 public class TicketServlet extends HttpServlet {
